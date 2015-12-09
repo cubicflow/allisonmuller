223e1968a9cd423de6eecba5a0528dd392010bc8
BackdoorDan <d.anmcn.air@gmail.com>
2015-12-09 23:23:12 +0900

    added toggler to services page

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,19 @@ $(document).foundation();
 //
 // Custom JS
 // --------------------------------------------------
+
+var toggleBtn = $('.toggle-btn');
+if(toggleBtn.length > 0){
+	
+	toggleBtn.on('click', function(){
+		
+		var idToToggle = $(this).attr('toggle-target');
+		
+		$(idToToggle).toggleClass('active');
+		$(this).toggleClass('active');
+		
+	});
+	
+} else {
+	// no toggle btn
+}
